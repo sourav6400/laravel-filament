7248e1d16147169108d42232916686f75e902cbe
sourav6400 <imsourav6400@gmail.com>
2023-06-05 14:56:30 +0600

    first commit

---
+++ app/Filament/Resources/CustomerResource/Pages/ListCustomers.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Resources\CustomerResource\Pages;
+
+use App\Filament\Resources\CustomerResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListCustomers extends ListRecords
+{
+    protected static string $resource = CustomerResource::class;
+
+    protected function getActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+
+    
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            CustomerResource\Widgets\StatsOverview::class,
+        ];
+    }
+}
---
+++ resources/js/Pages/Test/Test.jsx
@@ -0,0 +1,10 @@
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head } from '@inertiajs/react';
+
+export default function Test() {
+    return (
+            <div className="py-12">
+                <h1>Test</h1>
+            </div>
+    );
+}
